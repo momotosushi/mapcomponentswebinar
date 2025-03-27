027f279a927b9ba75bd8a6f89fecc61b5300369e
Moritz Elfeld <moritz.elfeld@wheregroup.com>
2025-03-27 09:05:25 +0100

    Test

---
+++ src/Layers/OSMLayer.jsx
@@ -0,0 +1,6 @@
+export default function OSMLayer() {
+    return(
+      <>
+      </>
+    )
+    }
---
+++ src/Layers/ProjectLayers.jsx
@@ -0,0 +1,110 @@
+import { 
+  MlGeoJsonLayer , 
+  useAddProtocol,  
+  CSVProtocolHandler,
+  Sidebar, 
+  LayerListItem,
+  LayerList
+} from "@mapcomponents/react-maplibre";
+import monitoringAreas from "./monitoringAreas.json";
+
+export default function ProjectLayers() {
+  useAddProtocol({
+    protocol: "csv",
+    handler: CSVProtocolHandler,
+  });
+
+  return (
+    <>
+      <Sidebar open={true} name={"Layers"} >
+      <MlGeoJsonLayer
+                geojson={monitoringAreas}  
+                options={{
+                  paint: {
+                    "fill-color": "#5353ec",
+                    "fill-opacity": 0.5,
+                    "fill-outline-color": "#000",
+                  },
+                }}
+              />
+        <MlGeoJsonLayer
+              layerId="samples"
+              type="circle"
+              options={{
+                source: {
+                  type: "geojson",
+                  data: "csv://sources/samples.csv",
+                },
+                paint: {
+                  "circle-color": "#22BB5D",
+                  "circle-stroke-width": 1,
+                },
+              }}
+              labelProp="id"
+              labelOptions={{
+                layout: {
+                  "text-size": {
+                    stops: [
+                      [13, 15],
+                      [22, 60],
+                    ],
+                  },
+                },
+                minzoom: 13,
+              }}
+        />            
+        <LayerList>
+          <LayerListItem
+            configurable={true}
+            type="layer"
+            name="Monitoring areas"
+            layerComponent={
+              <MlGeoJsonLayer
+                geojson={monitoringAreas}  
+                options={{
+                  paint: {
+                    "fill-color": "#5353ec",
+                    "fill-opacity": 0.5,
+                    "fill-outline-color": "#000",
+                  },
+                }}
+              />
+            }
+          />
+          <LayerListItem
+            name="Samples"
+            configurable={true}         
+            layerComponent={
+              <MlGeoJsonLayer
+              layerId="samples"
+              type="circle"
+              options={{
+                source: {
+                  type: "geojson",
+                  data: "csv://sources/samples.csv",
+                },
+                paint: {
+                  "circle-color": "#22BB5D",
+                  "circle-stroke-width": 1,
+                },
+              }}
+              labelProp="id"
+              labelOptions={{
+                layout: {
+                  "text-size": {
+                    stops: [
+                      [13, 15],
+                      [22, 60],
+                    ],
+                  },
+                },
+                minzoom: 13,
+              }}
+              />
+            }
+          />
+        </LayerList>  
+      </Sidebar>
+    </>
+  );
+}
