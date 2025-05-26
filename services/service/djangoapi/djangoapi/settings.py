5ed5a97e8e8db5d4af47f9c975a1ab5b3a03e30e
Moritz Elfeld <moritz.elfeld@wheregroup.com>
2025-05-26 12:09:17 +0200

    added rest_framework to settings

--- services/service/djangoapi/djangoapi/settings.py
+++ services/service/djangoapi/djangoapi/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'djangoapi',
     'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- services/service/djangoapi/djangoapi/views/GetInfo.py
+++ services/service/djangoapi/djangoapi/views/GetInfo.py
@@ -1,4 +1,3 @@
-
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
